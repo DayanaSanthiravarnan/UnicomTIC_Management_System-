83a1abf86ee0cd8323bb194649ac60db3523df4c
DayanaSanthiravarnan <dayanasanthiravarnan@gmail.com>
2025-06-10 16:11:23 +0530

    Add project files.

---
+++ UnicomTIC_Management/Datas/Dbconfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicomTIC_Management.Datas
+{
+    internal class Dbconfig
+    {
+            
+            
+        private static string connectionString = "Data Source=UnicomticManagementDB.db;Version=3;";
+
+        public static SQLiteConnection GetConnection()
+        {
+            var conn = new SQLiteConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
+    
+
